2e4cf31f2cf9156d18ea06decbc52b54782cbac4
Solomon Israel <solomonisrael0012@gmail.com>
2023-12-27 14:01:54 +0100

    Add routes & create Pages

---
+++ clients/src/pages/Signin.jsx
@@ -0,0 +1,6 @@
+
+export default function Signin() {
+  return (
+    <div className="text-blue-500">Signin</div>
+  )
+}
---
+++ clients/src/pages/Signup.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Signup() {
+  return (
+    <div className="text-blue-400">Signup</div>
+  )
+}
